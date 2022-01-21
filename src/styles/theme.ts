f7d9fe74dea56ac2a99e859b5dd361e71c83193d
Mardeson Pereira <maxpb777@gmail.com>
2022-01-21 07:34:14 -0300

    chore: add styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    header: '#00aaf2',
+    shape: '#FFFFFF',
+    placeholder: '#cecece',
+  },
+
+  fonts: {
+    primary_400: 'Inter_400Regular',
+    primary_500: 'Inter_500Medium',
+  },
+};
